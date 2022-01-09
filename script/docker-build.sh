77e63879aba0fa22ccfd16782b1f25e5fd55227b
John Morrice <john.morrice.developer@gmail.com>
2022-01-09 12:08:58 +0000

    fix docker tag

--- script/docker-build.sh
+++ script/docker-build.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 set -e
-docker build . -f Dockerfiles/Dockerfile.my-solana-base -t my-solana-base.latest
+docker build . -f Dockerfiles/Dockerfile.my-solana-base -t my-solana-base:latest
 docker build . -f Dockerfiles/Dockerfile.my-solana-hello-world -t my-solana-hello-world:latest
