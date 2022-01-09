59f193660ff53bbacaab0cbad7723daa7bae8502
John Morrice <john.morrice.developer@gmail.com>
2022-01-09 11:23:07 +0000

    clone the submodules

--- vagrant/setup_vagrant_home.sh
+++ vagrant/setup_vagrant_home.sh
@@ -4,5 +4,5 @@ set -e
 PAT=$(cat github.pat)
 git config --global user.email "$GIT_USER_EMAIL"
 git config --global user.name "$GIT_USER_NAME"
-git clone "https://${GITHUB_USERNAME}:${PAT}@github.com/johnny-morrice/solana-hello-world.git"
+git clone --recurse-submodules "https://${GITHUB_USERNAME}:${PAT}@github.com/johnny-morrice/solana-hello-world.git"
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
