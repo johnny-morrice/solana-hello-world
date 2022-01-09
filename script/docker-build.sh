cc882cb2249eab86d34e1eda1b66b2cb43b59553
John Morrice <john.morrice.developer@gmail.com>
2022-01-09 11:43:46 +0000

    fix docker script

--- script/docker-build.sh
+++ script/docker-build.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-docker build . -f Dockerfiles/Dockerfile.solanabase -t my-solana-base.latest
-docker build . -f Dockerfiles/Dockerfile.app -t my-solana-hello-world:latest
+docker build . -f Dockerfiles/Dockerfile.my-solana-base -t my-solana-base.latest
+docker build . -f Dockerfiles/Dockerfile.my-solana-hello-world -t my-solana-hello-world:latest
